85cf5882c00190a50e17e98a745e80527d0d2b33
tired1student <2782969439@qq.com>
2025-05-28 17:49:24 +0800

    white box testing

--- src/test/java/GraphProcessorTest.java
+++ src/test/java/GraphProcessorTest.java
@@ -3,8 +3,11 @@ package test.java;
 import static org.junit.jupiter.api.Assertions.*;
 
 import main.java.GraphProcessor;
+import main.java.utils.FileUtils;
+import main.java.utils.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 class GraphProcessorTest {
 
     private GraphProcessor graphProcessor;
@@ -12,12 +15,14 @@ class GraphProcessorTest {
     @BeforeEach
     public void setUp() {
         graphProcessor = new GraphProcessor();
-        // 在这里初始化图结构，根据实际需求添加单词和边
-        String[] words = {"but", "the", "data", "example"};
-        graphProcessor.generateGraph(words);
-        graphProcessor.generateGraph(new String[]{"the", "data"});
+        // 初始化图结构
+        String text = FileUtils.readFile("./resources/Easy Test.txt");
+        text = String.join(" ", StringUtils.preprocess(text)); // 使用 String.join 将数组转换为字符串
+        graphProcessor.generateGraph(text.split(" "));
+
     }
 
+    // queryBridgeWords 黑盒测试
     @Test
     public void testNoBridgeWords() {
         String result = graphProcessor.queryBridgeWords("but", "the");
@@ -43,4 +48,25 @@ class GraphProcessorTest {
     }
 
 
+    // calcShortestPath 白盒测试
+    @Test
+    public void testNonexistentNodes() {
+        // 测试用例 1
+        String result = graphProcessor.calcShortestPath("but", "butthe");
+        assertEquals("No word1 or word2 in the graph!", result);
+    }
+
+    @Test
+    public void testNoPathFound() {
+        // 测试用例 2
+        String result = graphProcessor.calcShortestPath("again", "it");
+        assertEquals("again to it is unreachable.", result);
+    }
+
+    @Test
+    public void testPathFound() {
+        // 测试用例 3
+        String result = graphProcessor.calcShortestPath("but", "so");
+        assertEquals("Shortest path from but to so: but -> the -> data -> so (Length: 3.0)", result);
+    }
 }
